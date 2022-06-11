60c46148d685007e1de22e7f2e34201d79c9835d
nick <smz1680@gmail.com>
2022-06-10 17:01:58 -0700

    fixed header

--- src/components/header/Nav.js
+++ src/components/header/Nav.js
@@ -1,12 +1,13 @@
 import React from "react"
 import Logo from "./Logo"
+import './Nav.css'
 
 import LinkList from "./LinkList"
 const Nav = ()=> {
   return (
 		<div className = "navigation">
 			<div className = "container-fluid">
-				<div className = "row">
+				<div className = "row nav-wrapper">
 					<Logo />
 					<LinkList />
 				</div>
